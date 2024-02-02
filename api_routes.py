8a7bd8ad4f7b3a93ffbd7fa915d466710169ce8d
Kevin Bruton <kevin.jose.bs@gmail.com>
2024-02-02 11:36:03 +0100

    feat: ssl api communication with client authentication

--- api_routes.py
+++ api_routes.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
-from routers import positions
+from typing import Annotated
+from fastapi import FastAPI, Header, HTTPException, Request, Response, status
+from routers import trades
+from routers import strategies
 from utils.config import get_config_value
 
 openapi_url = '/openapi.json' if get_config_value('enable_openapi_docs') else ''
 app = FastAPI(openapi_url=openapi_url)
-app.include_router(positions.router)
 
-@app.get("/")
+@app.middleware('http')
+async def client_authentication(request: Request, call_next):
+    client_id = request.headers.get('ClientId')
+    if client_id != get_config_value('client_id'):
+        return Response(status_code=status.HTTP_403_FORBIDDEN)
+    return await call_next(request)
+
+app.include_router(trades.router)
+app.include_router(strategies.router)
+
+@app.get("/hello")
 def read_root():
     return {"Hello": "World"}
--- api_server.py
+++ api_server.py
@@ -1,5 +1,11 @@
 from uvicorn import run
+from utils.config import get_config_value
 
 def run_api_server():
   print('Starting API server...')
-  run("api_routes:app", host="0.0.0.0", port=8000)
+  run("api_routes:app", \
+      host="0.0.0.0", \
+      port=443, \
+      ssl_keyfile=get_config_value('ssl_privkey'), \
+      ssl_certfile=get_config_value('ssl_fullchain')
+    )
